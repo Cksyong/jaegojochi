0d962c392c982d6336ae19823ab7039ef59ab65b
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-06-30 13:32:58 +0900

    메인, 디테일 완성

--- lib/stock_Detail_Info.dart
+++ lib/stock_Detail_Info.dart
@@ -13,28 +13,50 @@ class _stock_Detail_InfoState extends State<stock_Detail_Info> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('재고 수정'),
+        title: const Text('상세페이지'),
       ),
       body: Column(
         children: [
           Container(
-              padding: const EdgeInsets.all(20),
+              padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
               child: Image.asset('assets/image/takoyaki.jpg')),
           Container(
             color: Colors.yellow,
             margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-            child: const Text('상품명',style: TextStyle(fontSize: 30, fontWeight:FontWeight.bold),),
-          )
-        ],
+            child: const Text(
+              '상품명',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(width: 0, height: 100),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                color: Colors.blue,
+                alignment: Alignment.centerRight,
+                child: const Text('8',
+                    style:
+                        TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Text('EA',
+                    style:
+                        TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+              )],),],),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const manage_Stock_page()));
+        },
+        child: const Icon(Icons.edit_calendar_rounded),
       ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const manage_Stock_page()));
-          },
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
     );
   }
 }
